458094614ab22f4ab732c753628d6c669e1f9866
David Roeca <david.roeca@yale.edu>
2015-08-31 03:20:29 -0400

    added problems to comments

--- python/p4.py
+++ python/p4.py
@@ -1,3 +1,11 @@
+"""
+Problem 4:
+A palindromic number reads the same both ways. The largest palindrome made from
+the product of two 2-digit numbers is 9009 = 91 x 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
 def is_palindrome(num):
     str_num = str(num)
     if int(str_num) == int(str_num[::-1]):
--- python/p8.py
+++ python/p8.py
@@ -1,4 +1,5 @@
 """
+Problem 8:
 The four adjacent digits in the 1000-digit number that have the greatest product
 are 9 x 9 x 8 x 9 = 5832
 

9c81cc4c18a0cca68c543ea84c82bcbfe03629ac
David Roeca <david.roeca@yale.edu>
2015-09-10 01:41:40 -0400

    added problem 15

---
+++ python/p015.py
@@ -0,0 +1,21 @@
+"""
+Problem 15:
+Starting in the top left corner of a 2x2 grid, and only being able to
+move to the right and down, there are exactly 6 routes to the bottom
+right corner.
+How many such routes are there through a 20x20 grid?
+"""
+
+def factorial(number):
+    return reduce(lambda x, y: x * y, xrange(1, number + 1))
+
+def get_num_routes(dimension):
+    dim_fact = factorial(dimension)
+    return factorial(2 * dimension) / (dim_fact * dim_fact)
+
+def main():
+    dimension = 20
+    print get_num_routes(dimension)
+
+if __name__ == '__main__':
+    main()

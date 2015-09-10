4d99a6749c16fbd0a9f2f98fccee88d2d1b13661
David Roeca <david.roeca@yale.edu>
2015-09-10 01:54:09 -0400

    added problem 16

---
+++ python/p016.py
@@ -0,0 +1,20 @@
+"""
+Problem 16:
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+"""
+
+def sum_dig(num):
+    digit_sum = 0
+    while (num):
+        digit_sum += num % 10
+        num = num / 10
+    return digit_sum
+
+def main():
+    two2thousandth = 1 << 1000
+    print sum_dig(two2thousandth)
+
+if __name__ == '__main__':
+    main()
